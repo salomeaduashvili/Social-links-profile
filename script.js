414c53f15b25122c6a1a75816a971ea3d2cd8eee
salome aduashvili <salome.sali9909@gmail.com>
2025-11-18 05:54:48 +0400

    create html css and js files

---
+++ script.js
@@ -0,0 +1,23 @@
+const git = document.getElementById("git");
+git.addEventListener("click", () => {
+  window.open("http://github.com/");
+});
+const fm = document.getElementById("fm");
+fm.addEventListener("click", () => {
+  window.open("http://frontendmentor.io/");
+});
+
+const linkedIn = document.getElementById("linkedIn");
+linkedIn.addEventListener("click", () => {
+  window.open("http://linkedin.com/");
+});
+
+const x = document.getElementById("twitter");
+x.addEventListener("click", () => {
+  window.open("https://x.com/");
+});
+
+const insta = document.getElementById("insta");
+insta.addEventListener("click", () => {
+  window.open("https://www.instagram.com/");
+});
